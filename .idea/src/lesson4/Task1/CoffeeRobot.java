68c10551323642ac179aeed418953544ebaacdfc
Roman Taras <taras-chemist@ukr.net>
2020-03-21 23:32:25 +0200

    my lesson 4 hometasks

---
+++ .idea/src/lesson4/Task1/CoffeeRobot.java
@@ -0,0 +1,14 @@
+package lesson4.Task1;
+
+public class CoffeeRobot extends Robot {
+    @Override
+    public void work() {
+        System.out.println("I am CoffeeRobot I`m just making coffee");
+    }
+
+    @Override
+    public String toString() {
+        return "CoffeeRobot \"I am CoffeeRobot I`m just making coffee\"";
+    }
+}
+
---
+++ .idea/src/lesson4/Task1/RobotCocker.java
@@ -0,0 +1,14 @@
+package lesson4.Task1;
+
+public class RobotCocker extends Robot {
+    @Override
+    public void work() {
+        System.out.println("I am robotCocker i making delitious food!!!");
+    }
+
+    @Override
+    public String toString() {
+        return "RobotCocker \"I am robotCocker i making delitious food!!!\" ";
+    }
+}
+
