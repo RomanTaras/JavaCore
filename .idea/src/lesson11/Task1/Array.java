0b5635d3aeb3d8674ea169c356b28b63b1075047
Roman Taras <taras-chemist@ukr.net>
2020-05-19 17:33:48 +0300

    Array list statement simplified

--- .idea/src/lesson11/Task1/Array.java
+++ .idea/src/lesson11/Task1/Array.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Array {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<Integer>();
+        List list = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
             list.add(i, (int) ((Math.random() * 101)));
         }
