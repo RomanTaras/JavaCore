7f8b7dd52cbb8ca173c3f5c459f3ae3fcdfd4a44
Roman Taras <taras-chemist@ukr.net>
2020-05-19 21:38:51 +0300

    correct realization of palindrome

--- .idea/src/lesson10/Task1/Palindrome.java
+++ .idea/src/lesson10/Task1/Palindrome.java
@@ -1,17 +1,38 @@
 package lesson10.Task1;
 
+import java.util.*;
+
 public class Palindrome {
+    private static String text;
+
     public static void main(String[] args) {
-        String first = "SpOOn";
-        String second = "rOToR";
-        String m1 = first.toUpperCase();
-        String m2 = second.toUpperCase();
-        int p1 = m1.length();
-        int p2 = m2.length();
-        if (p1 == p2) {
-            System.out.println("Entered verbs are palindrome");
-        } else {
-            System.out.println("Entered verbs are not palindrome");
+        System.out.println("Please enter two words contains of 5 letters for palindrome ckeck = ");
+        Scanner scanner = new Scanner(System.in);
+        String first = scanner.next().toLowerCase();
+        String second = scanner.next().toLowerCase();
+
+        if (first.toCharArray().length == 5 && second.toCharArray().length == 5) {
+
+            String reverse1 = new StringBuilder(new String(first)).reverse().toString();
+            String reverse2 = new StringBuilder(new String(second)).reverse().toString();
+
+
+            if (first.matches(reverse1)) {
+                System.out.println(first + " is palindrome");
+            } else {
+                System.out.println(first + " is not palindrome");
+            }
+
+            if (second.matches(reverse2)) {
+                System.out.println(second + " is palindrome");
+            }
+            else {
+                System.out.println(second + " is not palindrome");
+            }
+        }
+        else {
+            System.out.println("entered word doesn`t contains 5 letters");
         }
     }
 }
+
